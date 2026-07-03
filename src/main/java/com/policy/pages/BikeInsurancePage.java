ad0cec90b8f34b901c680eaf7695eaa566b35bcd
Saravanakumar182 <saravanakumarsenbagavel@gmail.com>
2026-07-03 11:35:51 +0530

    updated structure

--- src/main/java/com/policy/pages/BikeInsurancePage.java
+++ src/main/java/com/policy/pages/BikeInsurancePage.java
@@ -1,4 +1,13 @@
 package com.policy.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class BikeInsurancePage {
+    private WebDriver driver;
+
+    public BikeInsurancePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
 }
--- src/main/java/com/policy/pages/TravelInsurancePage.java
+++ src/main/java/com/policy/pages/TravelInsurancePage.java
@@ -1,4 +1,13 @@
 package com.policy.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class TravelInsurancePage {
+    private WebDriver driver;
+
+    public TravelInsurancePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
 }
